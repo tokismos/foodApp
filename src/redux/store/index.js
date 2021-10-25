ae4fa3926007de27bebf3348c4a3891ed4ee289b
Tokismos <tokismos@gmail.com>
2021-10-25 03:22:33 +0100

    add filter screen

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import filterReducer from "../slicer/filterSlicer";
 import matchesReducer from "../slicer/MatchSlicer";
 
 export const store = configureStore({
   reducer: {
     matchStore: matchesReducer,
+    filterStore: filterReducer,
   },
 });
